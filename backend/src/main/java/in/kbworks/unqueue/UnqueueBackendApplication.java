008fcadec5dc8ad5e81dd1a688a8bd0d19db4cf5
Kishor <kishorbharti010@gmail.com>
2026-02-05 20:41:55 +0530

    Initialize Spring Boot backend (Java 17, Postgres, Security)

---
+++ backend/src/main/java/in/kbworks/unqueue/UnqueueBackendApplication.java
@@ -0,0 +1,13 @@
+package in.kbworks.unqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnqueueBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnqueueBackendApplication.class, args);
+	}
+
+}
